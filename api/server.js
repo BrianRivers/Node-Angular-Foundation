f9627c45e1a2248d8c18d2f2f9b3190924cc1e76
Lance Batson <lbatson@my.apsu.edu>
2013-11-25 11:27:27 -0600

    installed node-mysql adapter and created basic connection and query to db. updated .gitignore for all sftp-config files

--- api/server.js
+++ api/server.js
@@ -1,13 +1,32 @@
 var http = require('http'),
 	cors = require('cors'),
+	mysql = require('mysql'),
 	uuid = require('node-uuid'),
 	express = require('express'),
 	passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
 	LocalAPIKeyStrategy = require('passport-localapikey').Strategy;
 
+var connection = mysql.createConnection({
+	host		: 'localhost',
+	database	: 'pyrocms',
+	user		: 'pyrocms',
+	password	: '5cJzQDGqeKE8rdxb'
+});
+
 passport.use(new LocalStrategy(
 	function (username, password, done) {
+		console.log(username);
+		// // query binding and escaping for sql query
+		// var sql = 'SELECT * FROM default_famaster WHERE fama_asset = ??';
+		// var inserts = [username];
+		// sql = mysql.format(sql, inserts);
+		// connection.query(sql, function (err, rows, fields) {
+		// 	if (err)
+		// 		return done(null, false);
+		// 	else
+		// 		return done(null, rows[0]);
+		// });
 		if (username == 'test' && password == 'password')
 			return done(null, username);
 		else
@@ -51,6 +70,7 @@ app.configure(function () {
 });
 
 app.get('/', function (req, res) {
+	console.log('index');
 	res.json({
 		"status": [
 			{ "success": "success message" },
@@ -60,8 +80,18 @@ app.get('/', function (req, res) {
 	});
 });
 
+app.post('/post', function (req, res) {
+	res.json(req.body);
+});
+
 app.get('/unauthorized', function (req, res) {
-	res.json('Not Authorized');
+	res.json({
+		"status": [
+			{ "success": null },
+			{ "error": "Not Authorized" }
+		],
+		"data": null
+	});
 });
 
 app.post('/authenticate',
@@ -85,6 +115,15 @@ app.post('/keytest',
 		res.json('Valid API Key');
 	}
 );
+
+app.get('/dbtest', function (req, res) {
+	connection.query('SELECT * FROM default_fadeptmt', function(err, rows, fields) {
+		if (err)
+			res.json(err);
+		else
+			res.json(rows);
+	});
+});
 
 app.listen(3000, '127.0.0.1');
 console.log('Listening on port 3000');
