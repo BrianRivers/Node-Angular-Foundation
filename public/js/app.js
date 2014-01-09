4efc698d69e950c5066201c28b9f7f3ab906c5fc
Jacob Hilliard <jhilliard1@my.apsu.edu>
2014-01-09 16:29:32 -0600

    add route provider and user service for log in authentication

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,74 @@
-var app = angular.module('testApp', []);
+var app = angular.module('mainApp', ['ngRoute'])
+.factory('UserService', function() {
+  var loggedUser = {
+    username : "",
+    key : "",
+    firstName : "",
+    lastName : "",
+    email : "",
+    loggedIn : false
+  };
+  return loggedUser;
+})
+.run(['$rootScope', '$location', 'UserService', function($root, $location, User) {
+  $root.$on('$routeChangeSuccess', function(scope, curRoute, prevRoute) {
+    console.log(curRoute);
+    console.log(prevRoute);
+    if (!prevRoute) {
+      if (!curRoute.access.isFree && !User.loggedIn) {
+        $location.path('/');
+      }
+    } else {
+      if (!prevRoute.access.isFree && !User.loggedIn) {
+        $location.path('/');
+      }
+    }
+  });
+}]);
 
-app.controller('testController', ['$scope', '$http', function($scope, $http) {
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: 'mainPage.html',
+      access: { isFree: true }
+    })
+    .when('/secure', {
+      controller: 'secureCtrl',
+      templateUrl: 'secure.html',
+      access: { isFree: false }
+    })
+    .otherwise({ redirectTo: '/' });
+}]);
+
+app.controller('loginController', ['$scope', '$http', 'UserService', function($scope, $http, User) {
   $scope.submit = function() {
-    if (this.username && this.password) {
+    if (this.usernameInput && this.passwordInput) {
       var self = this;
       $http.post('http://localhost:3001/authenticate', {
-        username: this.username,
-        password: this.password
+        username: this.usernameInput,
+        password: this.passwordInput
       })
       .success(function(data) {
         if (data) {
-          console.log(data.meta.success);
-          self.submitted = 'Success!';
-        } else {
-          self.submitted = 'Not Authorized';
+          User.username = data.user.username;
+          User.key = data.user.key;
+          User.firstName = data.user.firstName;
+          User.lastName = data.user.lastName;
+          User.email = data.user.email;
+          User.loggedIn = true;
         }
       })
       .error(function(err) {
         console.log(err);
         self.submitted = 'Not Authorized';
       });
-      this.username = '';
-      this.password = '';
+      this.usernameInput = '';
+      this.passwordInput = '';
     }
   };
+}]);
+
+app.controller('secureCtrl', ['$scope', 'UserService', function($scope, User){
+
 }]);
