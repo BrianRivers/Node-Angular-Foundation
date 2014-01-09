acad3bc27b3fad0375bd2b0a2fb11aa7de674b49
Lance Batson <lbatson@my.apsu.edu>
2014-01-09 11:16:11 -0600

    updated basic angular setup to check against api for authentication. updated file formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,22 @@
-function testController($scope) {
-  $scope.username = null;
-  $scope.password = null;
+var app = angular.module('testApp', []);
+
+app.controller('testController', ['$scope', '$http', function($scope, $http) {
   $scope.submit = function() {
     if (this.username && this.password) {
+      var self = this;
+      $.post('http://localhost:3001/authenticate', {
+        username: this.username,
+        password: this.password
+      })
+      .success(function(data) {
+        console.log(data);
+        self.submitted = 'Success!';
+      })
+      .error(function(err) {
+        console.log(err);
+      });
       this.username = '';
       this.password = '';
-      this.submitted = 'Success!';
     }
   };
-}
+}]);
--- server.js
+++ server.js
@@ -1,50 +1,53 @@
 // require modules
 var http = require('http'),
-	cors = require('cors'),
-	express = require('express'),
-	passport = require('passport'),
-	fs = require('fs');
+  cors = require('cors'),
+  express = require('express'),
+  passport = require('passport'),
+  fs = require('fs');
 
 // create app
 var app = express();
 
 // configure app
 app.configure(function () {
-	app
-	.use(express.methodOverride())
-	.use(express.bodyParser())
-	.use(passport.initialize())
-	.use(cors())
-	.use(app.router)
-	// serve static website in public folder
-	.use(express.static( __dirname + '/public'))
-	// handle 404 not found
-	.use(function (req, res, next) {
-		res.status(404).send('404', {
-			url: req.originalUrl,
-			error: 'Not found'
-		});
-	})
-	.use(function (err, req, res, next){
-		console.error(err.stack);
-		res.json(500, 'Internal Error');
-	});
+  app
+  .use(express.methodOverride())
+  .use(express.bodyParser())
+  .use(passport.initialize())
+  .use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE']
+  }))
+  .use(app.router)
+  // serve static website in public folder
+  .use(express.static( __dirname + '/public'))
+  // handle 404 not found
+  .use(function (req, res, next) {
+    res.status(404).send('404', {
+      url: req.originalUrl,
+      error: 'Not found'
+    });
+  })
+  .use(function (err, req, res, next){
+    console.error(err.stack);
+    res.json(500, 'Internal Error');
+  });
 });
 
 // determine if inital setup should run
 try {
-	fs.statSync('test/userdata.json').isFile();
+  fs.statSync('test/userdata.json').isFile();
 }
 catch (e) {
-	// run initial db setup
-	var data = require('./app/data');
-	data.intialSetup(function (err, results) {
-		if (err) throw err;
-		fs.writeFile('test/userdata.json', JSON.stringify(results), function (err) {
-			if (err) throw err;
-			console.log('User created and written to data file');
-		});
-	});
+  // run initial db setup
+  var data = require('./app/data');
+  data.intialSetup(function (err, results) {
+    if (err) throw err;
+    fs.writeFile('test/userdata.json', JSON.stringify(results), function (err) {
+      if (err) throw err;
+      console.log('User created and written to data file');
+    });
+  });
 }
 
 
