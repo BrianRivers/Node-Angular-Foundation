be69be20df5fb4c5110b709ebe177f51de4a74a6
Jacob Hilliard <jhilliard1@my.apsu.edu>
2014-02-11 12:54:54 -0600

    Figured out how to deal with specific rejection codes

--- public/js/services/httpInterceptorService.js
+++ public/js/services/httpInterceptorService.js
@@ -1,33 +1,38 @@
 angular.module('mainApp.services')
 .factory('httpInterceptor', ['$q', '$injector', '$location', 'localStorageService', function ($q, $injector, $location, localStorageService) {
-    return {
-      // override requests to add key to header from session info
-      request: function (config) {
-        if(localStorageService.get('session') !== null) {
-          config.headers = {
-            'Content-Type': 'application/json;charset=UTF-8',
-            'x-api-key': localStorageService.get('session').key.id
-          };
-          return config;
+  return {
+    // override requests to add key to header from session info
+    request: function (config) {
+      if(localStorageService.get('session') !== null) {
+        config.headers = {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'x-api-key': localStorageService.get('session').key.id
+        };
+        return config;
+      } else {
+        return config;
+      }
+    },
+    // response error handling and redirection
+    responseError: function(rejection) {
+      console.log(rejection.data.meta.message.code);
+      var sessionService = $injector.get("SessionService");
+      if(rejection.status === 401) {
+        $location.path('/');
+        sessionService.logout();
+        sessionService.makeAlert('danger', 'You must login first');
+      } else if(rejection.status === 403) {
+        $location.path('/');
+        sessionService.makeAlert('danger', 'You do not have access to this page');
+      } else if(rejection.status === 500) {
+        if(rejection.data.meta.message.code == "ER_DUP_ENTRY") {
+          sessionService.makeAlert('warning', 'This username or email is already in use.');
         } else {
-          return config;
+          $location.path('/');
+          sessionService.makeAlert('warning', 'There was a server error');
         }
-      },
-      // response error handling and redirection
-      responseError: function(rejection) {
-          var sessionService = $injector.get("SessionService");
-          if(rejection.status === 401) {
-            $location.path('/');
-            sessionService.logout();
-            sessionService.makeAlert('danger', 'You must login first');
-          } else if(rejection.status === 403) {
-            $location.path('/');
-            sessionService.makeAlert('danger', 'You do not have access to this page');
-          } else if(rejection.status === 500) {
-            $location.path('/');
-            sessionService.makeAlert('warning', 'There was a server error');
-          }
-          return $q.reject(rejection);
-        }
-    };
+      }
+      return $q.reject(rejection);
+    }
+  };
 }]);
